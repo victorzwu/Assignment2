e88d03688574fb4e04b111d1749a1fa89a973ce4
Victor Wu <victorwu@Victors-MacBook-Air.local>
2023-02-25 12:41:25 -0800

    add style and icons to edit screen, make check not show up if not over limit

--- screens/EditEntry.js
+++ screens/EditEntry.js
@@ -1,6 +1,8 @@
-import { View, Text, Pressable, Alert } from "react-native";
+import { View, Text, Pressable, Alert, StyleSheet } from "react-native";
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
 
 export default function EditEntry({ route, removeEntry, editOverLimit }) {
   const navigation = useNavigation();
@@ -16,40 +18,81 @@ export default function EditEntry({ route, removeEntry, editOverLimit }) {
   }
 
   return (
-    <View>
-      <Text>{route.params.item.text}</Text>
-      <Pressable
-        onPress={() =>
-          Alert.alert(
-            "Important",
-            "Are you sure you want to remove this item?",
-            [
-              {
-                text: "No",
-              },
-              { text: "Yes", onPress: () => remove(route.params.item.id) },
-            ]
-          )
-        }
-      >
-        <Text>Remove</Text>
-      </Pressable>
-      <Pressable
-        onPress={() =>
-          Alert.alert(
-            "Important",
-            "Are you sure you want to mark this item as reviewed?",
-            [
-              {
-                text: "No",
-              },
-              { text: "Yes", onPress: () => edit(route.params.item.id) },
-            ]
-          )
-        }
-      >
-        <Text>Check</Text>
-      </Pressable>
+    <View style={{ alignItems: "center" }}>
+      <View style={styles.container}>
+        <Text style={styles.text}>Calories: {route.params.item.calories}</Text>
+        <Text style={styles.text}>Description: {route.params.item.text}</Text>
+        <View style={styles.pressableContainer}>
+          <Pressable
+            style={styles.pressable}
+            onPress={() =>
+              Alert.alert(
+                "Important",
+                "Are you sure you want to remove this item?",
+                [
+                  {
+                    text: "No",
+                  },
+                  { text: "Yes", onPress: () => remove(route.params.item.id) },
+                ]
+              )
+            }
+          >
+            <FontAwesome5 name="trash" size={24} color="white" />
+          </Pressable>
+
+          {route.params.item.overLimit && (
+            <Pressable
+              style={styles.pressable}
+              onPress={() =>
+                Alert.alert(
+                  "Important",
+                  "Are you sure you want to mark this item as reviewed?",
+                  [
+                    {
+                      text: "No",
+                    },
+                    { text: "Yes", onPress: () => edit(route.params.item.id) },
+                  ]
+                )
+              }
+            >
+              <Entypo name="check" size={24} color="white" />
+            </Pressable>
+          )}
+        </View>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    borderColor: "slateblue",
+    backgroundColor: "slateblue",
+    borderRadius: 5,
+    borderWidth: 2,
+    width: 300,
+    margin: 10,
+    padding: 10,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "darkslateblue",
+    fontWeight: "bold",
+    fontSize: 20,
+    padding: 5,
+  },
+  pressableContainer: {
+    flexDirection: "row",
+    padding: 10,
+  },
+  pressable: {
+    padding: 10,
+    marginHorizontal: 5,
+    borderRadius: 5,
+    backgroundColor: "darkslateblue",
+  },
+});
