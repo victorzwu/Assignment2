3f635ddd7327f4517d79331ca9aa37d77b482a65
Victor Wu <victorwu@Victors-Air.hitronhub.home>
2023-02-24 17:32:28 -0800

    adding Stack Navigator and Tab Navigator

--- App.js
+++ App.js
@@ -3,16 +3,25 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {AddAnEntry, AllEntries, EditEntry, OverLimitEntries} from './Screens/AddAnEntry';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="All Entries" component={AllEntries} />
+      <Stack.Screen name="Over-limit Entries" component={OverLimitEntries} />
       </Stack.Navigator>
+      <Tab.Navigator>
+      <Stack.Screen name="All Entries" component={AllEntries} />
+      <Stack.Screen name="Over-limit Entries" component={OverLimitEntries} />
+      </Tab.Navigator>
     </NavigationContainer>
+    
+
   );
 }
 
