9ca305e1b981f7893af193652569974741692e60
Victor Wu <victorwu@Victors-Air.hitronhub.home>
2023-02-24 17:38:39 -0800

    remove Stack Navigator

--- App.js
+++ App.js
@@ -3,7 +3,10 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import {AddAnEntry, AllEntries, EditEntry, OverLimitEntries} from './Screens/AddAnEntry';
+import AddAnEntry from './screens/AddAnEntry';
+import AllEntries from './screens/AllEntries';
+import EditEntry from './screens/EditEntry';
+import OverLimitEntries from './screens/OverLimitEntries';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -11,13 +14,9 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-      <Stack.Screen name="All Entries" component={AllEntries} />
-      <Stack.Screen name="Over-limit Entries" component={OverLimitEntries} />
-      </Stack.Navigator>
       <Tab.Navigator>
-      <Stack.Screen name="All Entries" component={AllEntries} />
-      <Stack.Screen name="Over-limit Entries" component={OverLimitEntries} />
+      <Tab.Screen name="All Entries" component={AllEntries} />
+      <Tab.Screen name="Over-limit Entries" component={OverLimitEntries} />
       </Tab.Navigator>
     </NavigationContainer>
     
