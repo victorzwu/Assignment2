ae8729b02ba846d93c5b70542a098ab397691b32
Victor Wu <victorwu@Victors-Air.hitronhub.home>
2023-02-24 17:21:39 -0800

    import navigators

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
 
 export default function App() {
   return (
