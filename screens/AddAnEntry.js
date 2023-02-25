edda33265f28705f21c681c6092be8c56d724b34
Victor Wu <victorwu@Victors-Air.hitronhub.home>
2023-02-24 21:08:07 -0800

    check for invalid input

--- screens/AddAnEntry.js
+++ screens/AddAnEntry.js
@@ -1,31 +1,48 @@
-import { View, Text, TextInput, Pressable, Alert} from 'react-native'
-import { useState } from 'react';
-import React from 'react'
+import { View, Text, TextInput, Pressable, Alert } from "react-native";
+import { useState } from "react";
+import React from "react";
 
-export default function AddAnEntry({addEntry}) {
-    const [validCalories, setValidCalories] = useState(false);
-    const [validText, setValidText] = useState(false);
-    const [calories, setCalories] = useState("");
-    const [text, setText] = useState("");
+export default function AddAnEntry({ addEntry }) {
+  const [validCalories, setValidCalories] = useState(false);
+  const [validText, setValidText] = useState(false);
+  const [calories, setCalories] = useState("");
+  const [text, setText] = useState("");
 
-    function changedCalories(change)
-    {
-        
-    }
+  function changedCalories(change) {
+    setCalories(change);
+    calories !== "" && !isNaN(calories) && calories > 0 ? setValidCalories(true) : setValidCalories(false);
+  }
 
-    
+  function changedText(change) {
+    setText(change);
+    text !=="" ? setValidText(true) : setValidText(false);
+  }
 
   return (
     <View>
-    <Text >Calories</Text>
-    <TextInput
-              value={calories}
-              onChangeText={(change) => {
-                changedCalories(change);
-              }}
-            />
-      <Text >Description</Text>
-      <Pressable onPress = {()=> validCalories && validText ? addEntry(calories, text): Alert.alert("Invalid input", "Please check your input values")}><Text>Submit</Text></Pressable>
+      <Text>Calories</Text>
+      <TextInput
+        value={calories}
+        onChangeText={(change) => {
+          changedCalories(change);
+        }}
+      />
+      <Text>Description</Text>
+      <TextInput
+        value={text}
+        onChangeText={(change) => {
+          changedText(change);
+        }}
+      />
+      <Pressable
+        onPress={() =>
+          validCalories && validText
+            ? addEntry(calories, text)
+            : Alert.alert("Invalid input", "Please check your input values")
+        }
+      >
+        <Text>Submit</Text>
+      </Pressable>
     </View>
-  )
+  );
 }
